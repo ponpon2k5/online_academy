eb689d11ca53666f69513dd58900f6065b833962
Nguyễn Đức Duy <nguyenducduy25605@gmail.com>
2025-10-19 23:24:40 +0700

    merge code: add missing db.js

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import knex from 'knex';
 const db = knex({
         client: 'pg',
@@ -11,21 +10,4 @@ const db = knex({
         },
         pool: { min: 0, max: 15 }
     });
-=======
-import "dotenv/config";
-import knex from "knex";
-
-const db = knex({
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-  },
-  pool: { min: 0, max: 15 },
-});
-
->>>>>>> origin/feat/admin-categories
 export default db;
